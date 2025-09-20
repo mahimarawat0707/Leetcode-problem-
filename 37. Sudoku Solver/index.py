9c4423d2d98d8ca22115b076881a1cd3f6b106c4
Mahima <mahimarawat0707@gmail.com>
2025-09-20 22:53:56 +0530

    37. Sudoku Solver (Question type: Hard)

---
+++ 37. Sudoku Solver/index.py
@@ -0,0 +1,48 @@
+from typing import List
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empties = []
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    empties.append((r, c))
+                else:
+                    num = board[r][c]
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[(r // 3) * 3 + (c // 3)].add(num)
+
+        def backtrack(i=0):
+            if i == len(empties):
+                return True
+
+            r, c = empties[i]
+            b = (r // 3) * 3 + (c // 3)
+
+            for num in map(str, range(1, 10)):
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[b].add(num)
+
+                    if backtrack(i + 1):
+                        return True
+
+                    board[r][c] = "."
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[b].remove(num)
+
+            return False
+
+        backtrack()
