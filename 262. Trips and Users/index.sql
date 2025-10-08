3e9c5b776236c3785d772d6dfda0e85dfc8d8257
Mahima <mahimarawat0707@gmail.com>
2025-10-08 18:09:37 +0530

    262. Trips and Users (Question Type: Hard)

---
+++ 262. Trips and Users/index.sql
@@ -0,0 +1,12 @@
+SELECT
+    t.Request_at AS Day,
+    ROUND(
+        SUM(CASE WHEN t.Status LIKE 'cancelled%' THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS "Cancellation Rate"
+FROM Trips t
+JOIN Users c ON t.Client_Id = c.Users_Id AND c.Banned = 'No'
+JOIN Users d ON t.Driver_Id = d.Users_Id AND d.Banned = 'No'
+WHERE t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.Request_at
+ORDER BY t.Request_at;
