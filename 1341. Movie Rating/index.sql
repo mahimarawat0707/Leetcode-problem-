79e272100314d4631c1bf413fc40b154f8d7a100
Mahima <mahimarawat0707@gmail.com>
2025-11-02 21:26:10 +0530

    1341. Movie Rating (Question Type: Medium)

---
+++ 1341. Movie Rating/index.sql
@@ -0,0 +1,22 @@
+(
+    SELECT
+        name AS results
+    FROM Users u
+    JOIN MovieRating mr ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, name
+    LIMIT 1
+)
+
+UNION ALL
+
+(
+    SELECT
+        title AS results
+    FROM Movies m
+    JOIN MovieRating mr ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, title
+    LIMIT 1
+);
