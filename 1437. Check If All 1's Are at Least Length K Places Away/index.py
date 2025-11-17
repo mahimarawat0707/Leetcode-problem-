69e9b9e3c9a844acf9044e7d747134d61a8c2bca
Mahima <mahimarawat0707@gmail.com>
2025-11-17 19:09:01 +0530

    1437. Check If All 1's Are at Least Length K Places Away (Question Type: Easy)

---
+++ 1437. Check If All 1's Are at Least Length K Places Away/index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        prev = -1   # index of previous 1
+
+        for i, val in enumerate(nums):
+            if val == 1:
+                if prev != -1 and (i - prev - 1) < k:
+                    return False
+                prev = i
+
+        return True
