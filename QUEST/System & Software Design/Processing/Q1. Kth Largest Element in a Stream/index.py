69f86a355a2b33ad1ce937fa889521a66b572b9f
Mahima <mahimarawat0707@gmail.com>
2025-12-12 17:32:02 +0530

    Q1. Kth Largest Element in a Stream (Question Type: Easy)

---
+++ QUEST/System & Software Design/Processing/Q1. Kth Largest Element in a Stream/index.py
@@ -0,0 +1,21 @@
+from typing import List
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minheap = nums
+        heapq.heapify(self.minheap)
+
+        # Keep only k largest elements
+        while len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minheap, val)
+
+        if len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+
+        return self.minheap[0]
