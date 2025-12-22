31fe4e4d09c6873ac22fb68273c157b2d80f9e52
Mahima <mahimarawat0707@gmail.com>
2025-12-22 11:11:57 +0530

    quest

---
+++ QUEST/Data Structures and Algorithms/Association Slope/Prefix sum/Q2. Make Sum Divisible by P/index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        total_mod = sum(nums) % p
+        if total_mod == 0:
+            return 0
+
+        prefix_mod = 0
+        seen = {0: -1}  # prefix mod -> index
+        res = len(nums)
+
+        for i, num in enumerate(nums):
+            prefix_mod = (prefix_mod + num) % p
+            
+            target = (prefix_mod - total_mod) % p
+            if target in seen:
+                res = min(res, i - seen[target])
+            
+            seen[prefix_mod] = i
+
+        return res if res < len(nums) else -1
