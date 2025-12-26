cd9f8c38a9ba637911464bfde59ac214fe0a5a85
Mahima <mahimarawat0707@gmail.com>
2025-12-26 16:05:00 +0530

    Quest

---
+++ QUEST/Data Structures and Algorithms/Graph Theory Peaks/BFS/Q1. Find if Path Exists in Graph/index.py
@@ -0,0 +1,29 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+
+        # Build adjacency list
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+
+        def dfs(node: int) -> bool:
+            if node == destination:
+                return True
+
+            visited.add(node)
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor):
+                        return True
+
+            return False
+
+        return dfs(source)
