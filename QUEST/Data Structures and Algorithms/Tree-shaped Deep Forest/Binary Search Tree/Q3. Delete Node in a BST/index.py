39925d87ad647642b48221b547a33286e5da56cf
Mahima <mahimarawat0707@gmail.com>
2026-01-02 13:05:24 +0530

    quest

---
+++ QUEST/Data Structures and Algorithms/Tree-shaped Deep Forest/Binary Search Tree/Q3. Delete Node in a BST/index.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            # 🧹 Node found
+
+            # Case 1 & 2: One child or no child
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            # Case 3: Two children
+            # Find inorder successor (smallest in right subtree)
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, curr.val)
+
+        return root
