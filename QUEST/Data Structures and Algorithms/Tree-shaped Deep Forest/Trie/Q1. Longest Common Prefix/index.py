309176525e6769c4b4db1b6e796d2f689e6133ea
Mahima <mahimarawat0707@gmail.com>
2026-01-02 14:09:02 +0530

    quest

---
+++ QUEST/Data Structures and Algorithms/Tree-shaped Deep Forest/Trie/Q1. Longest Common Prefix/index.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, v: List[str]) -> str:
+        if not v:
+            return ""
+
+        v.sort()
+        first = v[0]
+        last = v[-1]
+        ans = ""
+
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return ans
+            ans += first[i]
+
+        return ans
