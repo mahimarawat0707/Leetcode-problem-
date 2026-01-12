fad611efe6767afe699c3fca79074b269c0df8b6
Mahima <mahimarawat0707@gmail.com>
2026-01-12 19:36:18 +0530

    1266. Minimum Time Visiting All Points (Question Type: Easy)

---
+++ 1266. Minimum Time Visiting All Points/index.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def minTimeToVisitAllPoints(self, p: List[List[int]]) -> int:
+        Ans = 0
+
+        for i in range(1, len(p)):
+            Ans += max(
+                abs(p[i][0] - p[i - 1][0]),
+                abs(p[i][1] - p[i - 1][1])
+            )
+
+        return Ans
+
+
+# Example usage 👇
+if __name__ == "__main__":
+    points = [[1, 1], [3, 4], [-1, 0]]
+    sol = Solution()
+    print(sol.minTimeToVisitAllPoints(points))
