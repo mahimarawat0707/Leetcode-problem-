10db36b7552005461073e39e6d8af28297db7d13
Mahima <mahimarawat0707@gmail.com>
2026-04-10 22:14:36 +0530

    deleted

--- QUEST/Data Structures and Algorithms/Association Slope/Prefix sum/Q1. Find the Highest Altitude/index.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        cur = 0
-        maxnum = 0
-        
-        for i in gain:
-            cur += i
-            maxnum = max(cur, maxnum)
-        return maxnum
